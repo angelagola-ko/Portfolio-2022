a03b4ed65c045da702e59575b58daed1263a2fc1
Angela Gola <angelagola.ko@gmail.com>
2022-08-20 11:08:07 -0400

    Fixed resume page and now it is displaying better

--- my-app/src/pages/Resume/Resume.js
+++ my-app/src/pages/Resume/Resume.js
@@ -2,8 +2,11 @@ import "./Resume.css";
 
 export default function Resume() {
     return (
+ <div>
      
-<embed src="https://docs.google.com/document/d/14y_j0p56Y93V5P5laXkNJiEpYEu7vZuCYMDfXyKxeLo/edit?usp=sharing"></embed>
+{/* <embed src="https://docs.google.com/document/d/14y_j0p56Y93V5P5laXkNJiEpYEu7vZuCYMDfXyKxeLo/edit?usp=sharing"></embed> */}
     
+<iframe src={require("./Resume.pdf")} style={{width:"100%", height:"600px"}}></iframe>
+ </div>    
     );
   }

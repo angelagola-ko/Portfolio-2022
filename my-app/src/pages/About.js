c8c64d08584af8ce5630aafb5e1015a6b3ea7305
Angela Gola <angelagola.ko@gmail.com>
2022-08-21 13:25:01 -0400

    Trying to add animations to homepage

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -7,6 +7,19 @@ const boxVariant = {
   hidden: { opacity: 0, scale: 0 }
 };
 
+const styling = {
+    width: "20%",
+    margin: "0px 30px 0px 0px",
+    border: "3px solid purple",
+    float: "left",
+    borderRadius: "25%",
+
+}
+
+const styling2 = {
+  width: "75%",
+  display:"flex"
+}
 const AboutMe = ({ num }) => {
 
   const control = useAnimation();
@@ -27,14 +40,18 @@ const AboutMe = ({ num }) => {
       variants={boxVariant}
       initial="hidden"
       animate={control}
+  
     >
-      <h1>Angela Gola</h1>
-      <h3>Previous Restaurant Events & Operations Manager turned Full-Stack Web Developer 2022. 
+      <h1 className="text-center py-4">Angela Gola</h1>
+      <img style={styling} src='https://user-images.githubusercontent.com/95582357/185797558-6e71c3af-224a-4415-9b05-232a32c79361.png'/>
+      <h3 style={styling2}>Previous Restaurant Events & Operations Manager turned Full-Stack Web Developer 2022. 
         With a desire to be challenged and contribute to code-based technology, I had to leave the mundane world of food & beverage management behind! 
-        
+        <br></br><br></br>
       My current experience is in HTML5, CSS3, JavaScript, React, jQuery, SQL, and Node, and I am open to learning new languages and technologies.
-
-      Extremely happy with my career change and excited for future opportunities and advancements in the industry.</h3>
+      Extremely happy with my career change and excited for future opportunities and advancements in the industry.<br>
+      </br><br></br>
+      In my spare time I enjoy being active and staying healthy. When I'm not out exploring, I take enjoyment in cooking, art, and photography. When I am out and about you can find me scuba diving, hiking, skiing or snowboarding, and checking countries off of my bucket list!
+      </h3>
       <br></br><br></br>
       
   
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,4 +1,11 @@
+// import { useSpring, animated } from 'react-spring';
+import React, { useState } from "react";
 
+ 
+// function App() {
+//   const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+//   return <animated.div style={props}>I will fade in</animated.div>
+// }
 const HomePage = (props) => {
     return (
       <div>

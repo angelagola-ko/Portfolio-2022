06e28b9658c54d95aeb4a13aff6858a20828e461
Angela Gola <angelagola.ko@gmail.com>
2022-08-20 22:55:50 -0400

    Adjusted styling and icons for cards

--- my-app/src/pages/Projects/Projects.js
+++ my-app/src/pages/Projects/Projects.js
@@ -14,8 +14,8 @@ function ProjectCards(props) {
         <div className="content">
               <p className="card-title">{props.name}</p>
               <p class="proj-icons-container">
-              <a href={props.github}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub Repo" /></a> 
-              <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live Application" /></a> 
+              <a href={props.github}><img class="project-icon" src="https://img.icons8.com/nolan/64/github.png" alt="GitHub Repo" /></a> 
+              <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/nolan/64/domain.png" alt="Live Application" /></a> 
        
              
              </p>

960d7018cf73fcf749daf692f8e157e280b10958
Angela Gola <angelagola.ko@gmail.com>
2022-09-03 13:32:04 -0400

    updated resume

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
      
 {/* <embed src="https://docs.google.com/document/d/14y_j0p56Y93V5P5laXkNJiEpYEu7vZuCYMDfXyKxeLo/edit?usp=sharing"></embed> */}
     
-<iframe src={require("./Resume.pdf")} style={{width:"100%", height:"900px"}}></iframe>
+<iframe src={require("./Resume9-22.pdf")} style={{width:"100%", height:"900px"}}></iframe>
  </div>    
     );
   }

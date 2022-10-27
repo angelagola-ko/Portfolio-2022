f85d24d5c805b52bfadcfd65e16028c2d46f59c6
Angela Gola <angelagola.ko@gmail.com>
2022-10-26 20:18:58 -0400

    Changed About Me page information

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,18 +8,25 @@ const boxVariant = {
 };
 
 const styling = {
-    width: "20%",
-    margin: "0px 30px 0px 0px",
-    border: "3px solid purple",
-    float: "left",
-    borderRadius: "25%",
+    width: "25%",
+    margin: "auto",
+    display: "block",
+    textAlign: "center",
+     border: "3px dotted teal",
+    // float: "left",
+    borderRadius: "50px",
 
 }
 
 const styling2 = {
-  width: "75%",
-  display:"flex"
+  width: "50%",
+  margin:"auto",
+  alignItems:"center",
+  justifyContent: "center",
+  paddingTop: "25px",
+  fontSize:"18px"
 }
+
 const AboutMe = ({ num }) => {
 
   const control = useAnimation();
@@ -29,30 +36,29 @@ const AboutMe = ({ num }) => {
     if (inView) {
       control.start("visible");
     } else {
-      control.start("hidden");
+      control.start("visible");
     }
   }, [control, inView]);
 
-  return (
+  return ( 
     <motion.div
       className="box"
       ref={ref}
       variants={boxVariant}
       initial="hidden"
       animate={control}
-  
-    >
-      <h1 className="text-center py-4">Angela Gola</h1>
-      <img style={styling} src='https://user-images.githubusercontent.com/95582357/185797558-6e71c3af-224a-4415-9b05-232a32c79361.png'/>
-      <h3 style={styling2}>Previous Restaurant Events & Operations Manager turned Full-Stack Web Developer 2022. 
-        With a desire to be challenged and contribute to code-based technology, I had to leave the mundane world of food & beverage management behind! 
+      
+      >
+      <h3 className="text-center py-4">About Me</h3>
+      <h3 style={styling2}>
+      Full-Stack Web Developer with a background in Management. Passionate about design, with a desire to be challenged and contribute to code-based technology. My current experience is in <b>HTML5, CSS3, JavaScript, React, GraphQL, jQuery, SQL, and Node</b>, and I am open to learning new languages and technologies. My goal is to become an effective and engaging part of a team that works to create user friendly experiences.
         <br></br><br></br>
-      My current experience is in HTML5, CSS3, JavaScript, React, jQuery, SQL, and Node, and I am open to learning new languages and technologies.
-      Extremely happy with my career change and excited for future opportunities and advancements in the industry.<br>
-      </br><br></br>
+     
+      <br></br>
       In my spare time I enjoy being active and staying healthy. When I'm not out exploring, I take enjoyment in cooking, art, and photography. When I am out and about you can find me scuba diving, hiking, skiing or snowboarding, and checking countries off of my bucket list!
       </h3>
       <br></br><br></br>
+      <img style={styling} src='https://user-images.githubusercontent.com/95582357/198161370-d033b0a7-5f2d-4783-b13b-ac8b50634a87.png'/>
       
   
       
